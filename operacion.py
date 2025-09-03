dfcc45e8769e53af4f8ba321bf3c14a118a14b7b
E22020 <110140130+E22020@users.noreply.github.com>
2025-09-03 11:39:24 -0400

    Add files via upload

---
+++ operacion.py
@@ -0,0 +1,9 @@
+# Operación simple
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("1 Sergio Brandon Flores Capriles")
+print("2 Omar Uzquiano")
+print("3 Elias Emanuel Callisaya Ruiz")
+print("4 Franco Quispe Josi")
---
+++ saludo.java
@@ -0,0 +1,9 @@
+public class saludo {
+ public static void main(String[] args) {
+ System.out.println("¡Hola, equipo!");
+ System.out.println("1 Sergio Brandon Flores Capriles");
+ System.out.println("2 Omar Uzquiano");
+ System.out.println("3 Elias Emanuel Callisaya Ruiz");
+ System.out.println("4 Franco Quispe Josi");
+ }
+}
